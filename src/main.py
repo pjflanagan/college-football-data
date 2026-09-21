38decb4b17f63e4f804697eb84e516d816c434b4
pflanagan <pflanagan@grubhub.com>
2026-09-21 11:35:22 -0400

    delete repo from computer

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 
-# Fetches the college football playoff data from last week
-# Fetches the college football playoff data from this week
+# Fetches the college football playoff data from last week (if we do not have it)
+# Fetches the college football playoff data from this week (and saves it to a file)
 
 # Calculates the total amount of change for each poll 
 # Calculates the most under and overrated
 
 # Outputs this information in various images
-#   - 1x Summary of each poll delta
+#   - 1x Summary of each poll delta, and cumulative average
 #   - 2x Most under and overrated, top 3
 #   - 2x Podium graph week to week with logo
 
